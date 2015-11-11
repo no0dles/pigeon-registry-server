123949590c903ffc1b94bf052a149f8c280863f6
Pascal <pascal.bertschi@hsr.ch>
2015-11-11 20:16:41 +0100

    added travis deployment

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var app = require('./app');
 
-for(var key in process.env) {
-  console.log(key + ': ' + process.env[key]);
-}
-
 var server = app.listen(app.get('port'), app.get('address'), function () {
   var host = server.address().address;
   var port = server.address().port;
