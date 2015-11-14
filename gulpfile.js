ecbb4a2ec14528bba57770a3ecd0245088329028
Pascal <pascal.bertschi@hsr.ch>
2015-11-14 12:27:09 +0100

    added gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var mocha = require('gulp-mocha');
 
 gulp.task('serve', function () {
-    nodemon({
-        script: 'index.js',
-        ext: 'js',
-        env: {
-            'NODE_ENV': 'development'
-        }
-    })
-    .on('restart', []);
+  nodemon({
+    script: 'index.js',
+    ext: 'js',
+    env: {
+      'NODE_ENV': 'development'
+    }
+  })
+  .on('restart', []);
+});
+
+gulp.task('test', function () {
+  return gulp.src('test/*.js', {read: false})
+    .pipe(mocha({reporter: 'nyan'}));
 });
 
 gulp.task('default', ['serve']);
