ea194fd539f48d07b2660b4cf579c203de4037f8
Pascal <pascal.bertschi@hsr.ch>
2015-11-14 12:31:59 +0100

    added gulp git

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var mocha = require('gulp-mocha');
+var git = require('gulp-git');
 
 gulp.task('serve', function () {
   nodemon({
@@ -16,6 +17,12 @@ gulp.task('serve', function () {
 gulp.task('test', function () {
   return gulp.src('test/*.js', {read: false})
     .pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('deploy', function(){
+  git.push('openshift', 'master', function (err) {
+    if (err) throw err;
+  });
 });
 
 gulp.task('default', ['serve']);
